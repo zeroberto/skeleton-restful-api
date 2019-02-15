6581a274e11588774d8abc0677058834425ddb31
José Roberto <jose.junior@conductor.com.br>
2019-02-15 09:05:52 -0300

    Initial commit

---
+++ src/main/java/br/com/zcreative/skeletonrestfulapi/SkeletonRestfulApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.zcreative.skeletonrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkeletonRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkeletonRestfulApiApplication.class, args);
+	}
+
+}
+
